361d9c0a3494a9aadfb7731f13240b7bc1cbe732
cardchess-leader <cardchess.leader@gmail.com>
2024-09-11 14:35:49 +0900

    Enabled CORS

--- functions/index.js
+++ functions/index.js
@@ -10,176 +10,153 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
 const { logger } = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
-// const { onDocumentCreated } = require("firebase-functions/v2/firestore");
-
-// The Firebase Admin SDK to access Firestore.
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
-const { HumorCategoryList, getDateInUTC, addDaysToDate } = require("./util/util");
+const cors = require("cors");
+const { HumorCategoryList, getDateInUTC, addDaysToDate, validateRequestBody } = require("./util/util");
 initializeApp();
 
+// Configure CORS to allow requests from multiple origins
+const corsHandler = cors({
+    origin: ["https://ddoh-admin-app--daily-dose-of-humors.us-central1.hosted.app", "http://localhost:3000"], // Specify allowed origins
+    methods: ["GET", "POST", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true, // Allow credentials if needed
+});
+
+exports.addDailyHumors = onRequest(async (req, res) => {
+    corsHandler(req, res, async () => {
+        try {
+            const payload = req.body;
+            const validatePayload = validateRequestBody(payload);
+            if (validatePayload.statusCode === 400) {
+                return res.status(400).json(validatePayload);
+            }
+
+            const db = getFirestore();
+            const dateDocRef = db.collection("Daily").doc(payload.date);
+
+            // Set the "date" field on the document, creating it if it doesn't exist
+            await dateDocRef.set({ date: payload.date }, { merge: true });
+
+            const docRef = dateDocRef.collection(payload.category).doc(payload.uuid);
+
+            // Add or set the document in the subcollection
+            await docRef.set(payload);
+
+            // Send a success response
+            res.status(200).json({ message: "Document added successfully." });
+        } catch (error) {
+            console.error("Error adding document:", error);
+            res.status(500).json({ error: "Could not add the document." });
+        }
+    });
+});
+
 exports.updateDailyHumors = onRequest(async (req, res) => {
-    try {
-        const payload = req.body;
-        // created_date should match the yyyy-mm-dd format
-        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-        if (typeof payload.date !== "string" || !dateRegex.test(payload.created_date)) {
-            return res.status(400).json({ error: "Invalid type for query date. Expected format 'yyyy-mm-dd'." });
+    corsHandler(req, res, async () => {
+        try {
+            const payload = req.body;
+            const validatePayload = validateRequestBody(payload);
+            if (validatePayload.statusCode === 400) {
+                return res.status(400).json(validatePayload);
+            }
+            const db = getFirestore();
+            const docRef = db.collection("Daily").doc(payload.date).collection(payload.category).doc(payload.uuid);
+
+            // Check if document exists
+            const docSnapshot = await docRef.get();
+            if (!docSnapshot.exists) {
+                return res.status(404).json({ error: "Document does not exist." });
+            }
+
+            // Update specific fields in the document
+            await docRef.update(payload);
+
+            // Send a success response
+            res.status(200).json({ message: "Document updated successfully." });
+        } catch (error) {
+            console.error("Error updating document:", error);
+            res.status(500).json({ error: "Could not update the document." });
         }
-        // author is nullable field
-        if ((typeof payload.author !== "string" || payload.author.trim() === "") && payload.author !== null) {
-            return res.status(400).json({ error: "Invalid type for author. Expected non-empty string." });
-        }
-
-        if (!HumorCategoryList.includes(payload.category)) {
-            return res.status(400).json({ error: "Invalid type for category. Expected one of the HumorCategory values." });
-        }
-
-        if (typeof payload.context !== "string" || payload.context.trim() === "") {
-            return res.status(400).json({ error: "Invalid type for context. Expected non-empty string." });
-        }
-
-        // context_list should be an array of strings
-        // context_list is nullable field
-        if ((!Array.isArray(payload.context_list) || !payload.context_list.every(item => typeof item === "string")) && payload.context_list !== null) {
-            return res.status(400).json({ error: "Invalid type for context_list. Expected an array of strings." });
-        }
-
-        // created_date should match the yyyy-mm-dd format
-        if (typeof payload.created_date !== "string" || !dateRegex.test(payload.created_date)) {
-            return res.status(400).json({ error: "Invalid type for created_date. Expected format 'yyyy-mm-dd'." });
-        }
-
-        if (typeof payload.index !== "number" || !Number.isInteger(payload.index)) {
-            return res.status(400).json({ error: "Invalid type for index. Expected an integer." });
-        }
-
-        if ((typeof payload.punchline !== "string" || payload.punchline.trim() === "") && payload.punchline !== null) {
-            return res.status(400).json({ error: "Invalid type for punchline. Expected non-empty string." });
-        }
-
-        if (typeof payload.sender !== "string" || payload.sender.trim() === "") {
-            return res.status(400).json({ error: "Invalid type for sender. Expected non-empty string." });
-        }
-
-        if (typeof payload.source !== "string" || payload.source.trim() === "") {
-            return res.status(400).json({ error: "Invalid type for source. Expected non-empty string." });
-        }
-
-        if (typeof payload.uuid !== "string" || payload.uuid.trim() === "") {
-            return res.status(400).json({ error: "Invalid type for uuid. Expected non-empty string." });
-        }
-        const db = getFirestore();
-        const docRef = db.collection("Daily").doc(payload.date).collection(payload.category).doc(payload.uuid);
-
-        // Check if document exists
-        const docSnapshot = await docRef.get();
-        if (!docSnapshot.exists) {
-            return res.status(404).json({ error: "Document does not exist." });
-        }
-
-        // Update specific fields in the document
-        await docRef.update({
-            author: payload.author,
-            context: payload.context,
-            punchline: payload.punchline,
-            context_list: payload.context_list,
-            created_date: payload.created_date,
-            index: payload.index,
-            sender: payload.sender,
-            source: payload.source,
-        });
-
-        // Example: Update specific fields in the document
-        await docRef.update({
-            author: payload.author,
-            context: payload.context,
-            punchline: payload.punchline,
-            context_list: payload.context_list,
-            created_date: payload.created_date,
-            index: payload.index,
-            sender: payload.sender,
-            source: payload.source,
-        });
-
-        // Send a success response
-        res.status(200).json({ message: "Document updated successfully." });
-    } catch (error) {
-        console.error("Error updating document:", error);
-        res.status(500).json({ error: "Could not update the document." });
-    }
+    });
 });
 
 exports.getDailyHumors = onRequest(async (req, res) => {
-    try {
-        const requestedCate = req.query.category; // string
+    corsHandler(req, res, async () => {
+        try {
+            const requestedCate = req.query.category; // string
 
-        // Category validation
-        if (requestedCate && !HumorCategoryList.includes(requestedCate)) {
-            logger.info("Invalid category.");
-            return res.status(400).json({ error: "Invalid category" });
-        }
+            // Category validation
+            if (requestedCate && !HumorCategoryList.includes(requestedCate)) {
+                logger.info("Invalid category.");
+                return res.status(400).json({ error: "Invalid category" });
+            }
 
-        let dailySnapshot;
-        if (req.query.date) {
-            dailySnapshot = await getFirestore()
-                .collection("Daily")
-                .where("date", "=", req.query.date)
-                .get();
-        } else {
-            // Get today's date in UTC format (yyyy-mm-dd)
-            const todayDate = getDateInUTC(new Date());
-            const sevenDaysAgoDate = getDateInUTC(addDaysToDate(new Date(), -7));
-            // Push the new message into Firestore using the Firebase Admin SDK.
-            dailySnapshot = await getFirestore()
-                .collection("Daily")
-                .where("date", ">", sevenDaysAgoDate)  // Start date filter
-                .where("date", "<=", todayDate)    // End date filter
-                .get();
-        }
+            let dailySnapshot;
+            if (req.query.date) {
+                dailySnapshot = await getFirestore()
+                    .collection("Daily")
+                    .where("date", "=", req.query.date)
+                    .get();
+            } else {
+                // Get today's date in UTC format (yyyy-mm-dd)
+                const todayDate = getDateInUTC(new Date());
+                const sevenDaysAgoDate = getDateInUTC(addDaysToDate(new Date(), -7));
+                // Push the new message into Firestore using the Firebase Admin SDK.
+                dailySnapshot = await getFirestore()
+                    .collection("Daily")
+                    .where("date", ">", sevenDaysAgoDate)  // Start date filter
+                    .where("date", "<=", todayDate)    // End date filter
+                    .get();
+            }
 
-        // Check if there are any matching date documents
-        if (dailySnapshot.empty) {
-            logger.info("No matching date documents found.");
-            return res.json({ humorList: [] });
-        }
+            // Check if there are any matching date documents
+            if (dailySnapshot.empty) {
+                logger.info("No matching date documents found.");
+                return res.json({ humorList: [] });
+            }
 
-        // Prepare to fetch subcollections
-        const promises = [];
+            // Prepare to fetch subcollections
+            const promises = [];
 
-        // Iterate over each date document to access the subcollection (e.g., DAD_JOKES)
-        dailySnapshot.forEach(doc => {
-            const dateDocId = doc.id; // ID of the date document
-            if (requestedCate == null) {
-                HumorCategoryList.forEach(cate => {
+            // Iterate over each date document to access the subcollection (e.g., DAD_JOKES)
+            dailySnapshot.forEach(doc => {
+                const dateDocId = doc.id; // ID of the date document
+                if (requestedCate == null) {
+                    HumorCategoryList.forEach(cate => {
+                        const subcollectionRef = getFirestore()
+                            .collection("Daily")
+                            .doc(dateDocId)
+                            .collection(cate)
+                            .orderBy("index");
+                        promises.push(subcollectionRef.get());
+                    });
+                } else {
                     const subcollectionRef = getFirestore()
                         .collection("Daily")
                         .doc(dateDocId)
-                        .collection(cate);
+                        .collection(requestedCate)
+                        .orderBy("index");
                     promises.push(subcollectionRef.get());
-                });
-            } else {
-                const subcollectionRef = getFirestore()
-                    .collection("Daily")
-                    .doc(dateDocId)
-                    .collection(requestedCate);
-                promises.push(subcollectionRef.get());
-            }
-        });
+                }
+            });
 
-        // Await all subcollection fetches
-        const snapshots = await Promise.all(promises);
+            // Await all subcollection fetches
+            const snapshots = await Promise.all(promises);
 
-        // Flatten and collect all documents from the subcollections
-        const dailyHumorList = snapshots.flatMap(snapshot =>
-            snapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data(),
-            }))
-        );
-        res.json({ humorList: dailyHumorList });
-    } catch (error) {
-        logger.error("Error fetching daily humors:", error);
-        res.status(500).json({ error: "Could not fetch humors..." });
-    }
+            // Flatten and collect all documents from the subcollections
+            const dailyHumorList = snapshots.flatMap(snapshot =>
+                snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data(),
+                    is_new: doc.date === getDateInUTC(new Date()),
+                }))
+            );
+            res.json({ humorList: dailyHumorList });
+        } catch (error) {
+            logger.error("Error fetching daily humors:", error);
+            res.status(500).json({ error: "Could not fetch humors..." });
+        }
+    });
 });
--- functions/util/util.js
+++ functions/util/util.js
@@ -13,8 +13,6 @@ const HumorCategoryList = [
 ];
 
 function getDateInUTC(date) {
-    // const today = new Date();
-
     // Extract year, month, and day in UTC (GMT+0)
     const year = date.getUTCFullYear();         // Year in UTC
     const month = String(date.getUTCMonth() + 1).padStart(2, "0");  // Month in UTC, adding 1 since it's zero-indexed
@@ -28,6 +26,64 @@ function addDaysToDate(date, numDaysToAdd) {
     return new Date(Date.now() + numDaysToAdd * 24 * 60 * 60 * 1000);
 }
 
+function validateRequestBody(requestBody) {
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (typeof requestBody.date !== "string" || !dateRegex.test(requestBody.date)) {
+        return { statusCode: 400, error: "Invalid type for query date. Expected format 'yyyy-mm-dd'." };
+    }
+    // author is not required
+    if (typeof requestBody.author !== "string") {
+        return { statusCode: 400, error: "Invalid type for author. Expected non-empty string." };
+    }
+
+    // category is required
+    if (!HumorCategoryList.includes(requestBody.category)) {
+        return { statusCode: 400, error: "Invalid type for category. Expected one of the HumorCategory values." };
+    }
+
+    // context is required
+    if (typeof requestBody.context !== "string" || requestBody.context.trim() === "") {
+        return { statusCode: 400, error: "Invalid type for context. Expected non-empty string." };
+    }
+
+    // context_list should be an array of strings
+    if (!Array.isArray(requestBody.context_list) || !requestBody.context_list.every(item => typeof item === "string")) {
+        return { statusCode: 400, error: "Invalid type for context_list. Expected an array of strings." };
+    }
+
+    // created_date is required
+    // created_date should match the yyyy-mm-dd format
+    if (typeof requestBody.created_date !== "string" || !dateRegex.test(requestBody.created_date)) {
+        return { statusCode: 400, error: "Invalid type for created_date. Expected format 'yyyy-mm-dd'." };
+    }
+
+    // index is required
+    if (typeof requestBody.index !== "number" || !Number.isInteger(requestBody.index)) {
+        return { statusCode: 400, error: "Invalid type for index. Expected an integer." };
+    }
+
+    // punchline is not required
+    if (typeof requestBody.punchline !== "string") {
+        return { statusCode: 400, error: "Invalid type for punchline. Expected non-empty string." };
+    }
+
+    // sender is required
+    if (typeof requestBody.sender !== "string" || requestBody.sender.trim() === "") {
+        return { statusCode: 400, error: "Invalid type for sender. Expected non-empty string." };
+    }
+
+    // source is required
+    if (typeof requestBody.source !== "string" || requestBody.source.trim() === "") {
+        return { statusCode: 400, error: "Invalid type for source. Expected non-empty string." };
+    }
+
+    // uuid is required
+    if (typeof requestBody.uuid !== "string" || requestBody.uuid.trim() === "") {
+        return { statusCode: 400, error: "Invalid type for uuid. Expected non-empty string." };
+    }
+    return { statusCode: 200 };
+}
+
 
 
 // Export both functions and constants
@@ -35,4 +91,5 @@ module.exports = {
     HumorCategoryList,
     getDateInUTC,
     addDaysToDate,
+    validateRequestBody
 };
